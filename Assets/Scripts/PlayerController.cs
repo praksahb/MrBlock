2e6589420e1a11b6599116818d577c5d08de4e10
praksahb <39479839+praksahb@users.noreply.github.com>
2022-05-11 04:14:16 +0530

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    public Rigidbody2D rb;
+    private Vector2 moveDirection;
+    public bool isSpacePressed;
+
+    void Update()
+    {
+        getInput();
+    }
+
+    void FixedUpdate()
+    {
+            move();
+    }
+
+    void getInput()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+        moveDirection = new Vector2(moveX, moveY).normalized;
+    }
+
+    void move()
+    {
+        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# MrBlock
+ 
